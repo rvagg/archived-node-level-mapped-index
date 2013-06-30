8c9ec2eb2ce512028b8dd8f63f2dfbebee97690f
Rod Vagg <rod@vagg.org>
2013-06-30 12:54:45 -0700

    style change, update deps

--- level-mapped-index.js
+++ level-mapped-index.js
@@ -4,72 +4,76 @@ const mapReduce       = require('map-reduce')
 
     , mapReducePrefix = 'mi/'
 
-var register = function (db, mapDb, indexName, indexer) {
-      if (typeof indexName == 'function') {
-        indexer = indexName
+function register (db, mapDb, indexName, indexer) {
+  if (typeof indexName == 'function') {
+    indexer = indexName
 
-        if (typeof mapDb == 'string') {
-          indexName = mapDb
-          mapDb = mapReducePrefix + mapDb
-        } else
-          indexName = mapDb._prefix
-      }
+    if (typeof mapDb == 'string') {
+      indexName = mapDb
+      mapDb = mapReducePrefix + mapDb
+    } else
+      indexName = mapDb._prefix
+  }
 
-      var emit = function (id, value, emit) {
-            indexer(id, value, function (value) {
-              emit(value, id)
-            })
-          }
-        , mapper = mapReduce(db, mapDb, emit)
+  function emit (id, value, _emit) {
+    indexer(id, value, function (value) {
+      _emit(value, id)
+    })
+  }
 
-      db._mappedIndexes[indexName] = mapper
-      return db
-    }
+  db._mappedIndexes[indexName] = mapReduce(db, mapDb, emit)
+  return db
+}
 
-  , indexedStream = function (db, indexName, key, options) {
-      if (!db._mappedIndexes[indexName])
-        throw new Error('No such index: ' + indexName)
-      if (!options)
-        options = {}
-      options = xtend(options || {}, { range: [ String(key), '' ] })
-      var stream = db._mappedIndexes[indexName]
-        .createReadStream(options)
-        .pipe(transformStream(function (data, finish) {
-          db.get(data.value, function (err, value) {
-            if (err) return finish()
-            finish(null, { key: data.value, value: value })
-          })
-        }))
-      stream.on('end', function () {
-        process.nextTick(stream.emit.bind(stream, 'close'))
+function indexedStream (db, indexName, key, options) {
+  if (!db._mappedIndexes[indexName])
+    throw new Error('No such index: ' + indexName)
+
+  if (!options)
+    options = {}
+
+  options = xtend(options || {}, { range: [ String(key), '' ] })
+
+  var stream = db._mappedIndexes[indexName]
+    .createReadStream(options)
+    .pipe(transformStream(function (data, finish) {
+      db.get(data.value, function (err, value) {
+        if (err) return finish()
+        finish(null, { key: data.value, value: value })
       })
-      return stream
-    }
+    }))
 
-  , getBy = function (db, indexName, key, callback) {
-      var data = []
-      db.createIndexedStream(indexName, key)
-        .on('data', function (_data) {
-          data.push(_data)
-        })
-        .on('error', function (err) {
-          callback(err)
-          callback = null
-        })
-        .on('close', function () {
-          callback && callback(null, data)
-        })
-    }
+  stream.on('end', function () {
+    process.nextTick(stream.emit.bind(stream, 'close'))
+  })
 
-  , setup = function (db) {
-      if (db._mappedIndexes) return
+  return stream
+}
 
-      db._mappedIndexes      = {}
-      db.registerIndex       = register.bind(null, db)
-      db.createIndexedStream = indexedStream.bind(null, db)
-      db.getBy               = getBy.bind(null, db)
+function getBy (db, indexName, key, callback) {
+  var data = []
+  db.createIndexedStream(indexName, key)
+    .on('data', function (_data) {
+      data.push(_data)
+    })
+    .on('error', function (err) {
+      callback(err)
+      callback = null
+    })
+    .on('close', function () {
+      callback && callback(null, data)
+    })
+}
 
-      return db
-    }
+function setup (db) {
+  if (db._mappedIndexes) return
+
+  db._mappedIndexes      = {}
+  db.registerIndex       = register.bind(null, db)
+  db.createIndexedStream = indexedStream.bind(null, db)
+  db.getBy               = getBy.bind(null, db)
+
+  return db
+}
 
 module.exports = setup
